eab15c927fef09dda2b34a2c5622e668470668f4
Natan <zantanad.24@gmail.com>
2024-01-28 12:24:38 +0700

    updaate tugas

--- src/component/card.tsx
+++ src/component/card.tsx
@@ -2,6 +2,8 @@ import TShirt from '../assets/T_shirt.jpeg'
 import Hoodie from '../assets/hoodie.jpeg'
 import Poster from '../assets/Posters.jpeg'
 import Album from '../assets/Albums.jpeg'
+import yellowStar from '../assets/star_shine.svg'
+import grayStar from '../assets/star_gray.svg'
 export const Card = ({data}:any) => {
     const T_shirt = TShirt
     const Hoodies = Hoodie
@@ -11,20 +13,46 @@ export const Card = ({data}:any) => {
     return(
         <div className="flex flex-col justify-center items-center gap-y-[14px] w-[160px]">
             <div
-            className="p-[5px] ring-1 ring-gray-200"
+            className="flex justify-end p-[5px] ring-1 ring-gray-200"
             >
+                <div className='flex justify-center items-center font-bold py-[1px] px-[8px] bg-[#fa8072] absolute text-white'>
+                    Sale!
+                </div>
                 <img
                 className="w-[150px] h-[180px]"
                 src={data.category === 'T-shirt' ? T_shirt : data.category === 'Hoodies' ? Hoodies : data.category === 'Posters' ? Posters : Albums}
                 alt={data.category}
-                />
+                >
+                </img>
             </div>
             <div className="flex flex-col justify-center items-center">
                 <div className="font-semibold text-blue-500">
                     {data.name}
                 </div>
+                <div className='flex flex-row'>
+                    {
+                        Array(5).fill(0).map((e, i) => (
+                           data.Rate > i ? (
+                            <img key={Math.random()} src={yellowStar} alt='starshine'></img>
+                           ):
+                            (
+                             <img key={Math.random()} src={grayStar} alt='stargray'></img>
+                            )
+                        ))
+                    }
+                </div>
                 <div className="font-extrabold">
-                    ${(data.price).toFixed(2)}
+                    {data.Sale ? (
+                     <div>
+                        <span className='text-gray-500'>
+                            <s>
+                                ${(data.price).toFixed(2)}
+                            </s>
+                        </span>
+                        {' '}
+                        ${(data.Sale).toFixed(2)}
+                    </div>
+                    ): (data.price).toFixed(2)}
                 </div>
             </div>
             <button
